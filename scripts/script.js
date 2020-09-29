9330f9a28b7d0c193eac933bb7deee66099a9ae8
chirayu-joshi <chirayujoshi1189@gmail.com>
2020-09-29 21:51:33 +0530

    changed timer of project reveal date

--- scripts/script.js
+++ scripts/script.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(2020, 08, 30, 00, 00, 00);
+var deadline = new Date(2020, 09, 01, 00, 00, 00);
 initializeClock('clockdiv', deadline);
